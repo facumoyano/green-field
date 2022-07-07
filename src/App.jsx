ba0da85468d184fb92f2f9b2cc5468885b5c8fd7
facumoyano <89871383+facumoyano@users.noreply.github.com>
2022-07-07 12:06:21 -0300

    scrollToTop

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ScrollToTop from "./helpers/scrollToTop";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 
 function App() {
     return (
         <BrowserRouter>
+            <ScrollToTop />
             <Routes>
                 <Route path="/" index element={<Home />} />
                 <Route path="/contact" element={<Contact />} />
